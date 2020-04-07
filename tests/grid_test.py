62084daff5b29d2b7407f0059f0360e36e8656fa
kihong <khhan1106@gmail.com>
2020-04-07 17:26:15 +0900

    Add testcase (No implemented)

---
+++ tests/grid_test.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+class TestGrid(unittest.TestCase):
+
+    def init_random_grid(self):
+        # test something
+        pass
+
+    def test_make_grid(self):
+        # test something
+        pass
+
+    def test_dump_grid(self):
+        # test something
+        pass
+
+    def test_update_grid(self):
+        # test something
+        pass
+
+    def test_get_alive_neighbors(self):
+        # test something
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
