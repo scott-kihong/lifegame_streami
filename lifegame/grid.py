724045a652d8bfe7f4059692baff948830a82fd9
kihong <khhan1106@gmail.com>
2020-04-07 13:50:37 +0900

    Add lifegame logic

--- lifegame/grid.py
+++ lifegame/grid.py
@@ -1,8 +1,10 @@
-import random
+import copy
 import os
+import random
 
 from lifegame.cell import Cell
 from lifegame.constants import MIN_BOARD_HEIGHT, MIN_BOARD_WIDTH
+from lifegame.grid_file_readwriter import GridFileReadWriter
 
 
 class Grid:
@@ -22,9 +24,46 @@ class Grid:
             self.init_random_grid()
 
         elif os.path.exists(filename):
-            # Do read generation file and init a board
-            pass
+            grid_file_reader = GridFileReadWriter(filename)
+            self.row_size, self.col_size = grid_file_reader.init_grid(self.board)
+
         else:
             raise FileNotFoundError("File dose not exist.")
 
         return self.board
+
+    def update_board(self):
+        new_board = copy.deepcopy(self.board)
+
+        for row in range(self.row_size):
+            for column in range(self.col_size):
+                num_neighbors = self.get_alive_neighbors(row, column)
+
+                if self.board[row][column].get_status() is True:
+                    if num_neighbors < 2 or num_neighbors > 3:
+                        new_board[row][column].set_false()
+                    if num_neighbors == 2 or num_neighbors == 3:
+                        new_board[row][column].set_true()
+                else:
+                    if num_neighbors == 3:
+                        new_board[row][column].set_true()
+        self.board = new_board
+
+        return self.board
+
+    def get_alive_neighbors(self, row, col):
+        num_neighbors = 0
+
+        for pos_row in range(-1, 2):
+            for pos_col in range(-1, 2):
+                neighbor_row = row + pos_row
+                neighbor_col = col + pos_col
+                if (neighbor_row == row and neighbor_col == col) or \
+                    (neighbor_row < 0 or neighbor_row >= self.row_size) or \
+                    (neighbor_col < 0 or neighbor_col >= self.col_size):
+                    continue
+
+                if self.board[neighbor_row][neighbor_col].get_status():
+                    num_neighbors += 1
+
+        return num_neighbors
