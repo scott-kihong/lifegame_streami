bd9a6c21d0a04d2ad0460c162bab8b51a790b07e
kihong <khhan1106@gmail.com>
2020-04-07 12:16:47 +0900

    Add properties and methods of cell class

--- lifegame/cell.py
+++ lifegame/cell.py
@@ -1,4 +1,13 @@
 class Cell:
 
-    def __init__(self):
-        pass
+    def __init__(self, status):
+        self.status = status
+
+    def get_status(self):
+        return self.status
+
+    def set_true(self):
+        self.status = True
+
+    def set_false(self):
+        self.status = False
