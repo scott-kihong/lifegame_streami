9de7713f5b6af291d4110f1cf1d4b98389aa4ab3
kihong <khhan1106@gmail.com>
2020-04-07 12:24:12 +0900

    Add grid initialized functions

--- lifegame/grid.py
+++ lifegame/grid.py
@@ -1,5 +1,30 @@
+import random
+import os
+
+from lifegame.cell import Cell
+from lifegame.constants import MIN_BOARD_HEIGHT, MIN_BOARD_WIDTH
+
+
 class Grid:
 
     def __init__(self):
-        pass
+        self.board = []
+        self.row_size = MIN_BOARD_HEIGHT
+        self.col_size = MIN_BOARD_WIDTH
 
+    def init_random_grid(self):
+        for row in range(self.row_size):
+            col_list = [Cell(random.choice([True, False])) for _ in range(self.col_size)]
+            self.board.append(col_list)
+
+    def make_grid(self, filename=None):
+        if filename is None:
+            self.init_random_grid()
+
+        elif os.path.exists(filename):
+            # Do read generation file and init a board
+            pass
+        else:
+            raise FileNotFoundError("File dose not exist.")
+
+        return self.board
