3767f93925d1d17896013d84e6a836ed2416fee0
kihong <khhan1106@gmail.com>
2020-04-07 13:57:38 +0900

    Add print grid function

--- lifegame/grid_viewer.py
+++ lifegame/grid_viewer.py
@@ -1,4 +1,17 @@
+from lifegame.constants import STATUS_SYMBOL
+
+
 class GridViewer:
 
     def __init__(self):
-        pass
+        self.true_symbol = STATUS_SYMBOL['alive']
+        self.false_symbol = STATUS_SYMBOL['dead']
+
+    def print_grid(self, board):
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j].get_status():
+                    print(self.true_symbol, end='')
+                else:
+                    print(self.false_symbol, end='')
+            print('\r')
