6f2c9433b884260cc12858aaa08b8a43f0f884f7
kihong <khhan1106@gmail.com>
2020-04-07 12:19:50 +0900

    Add program argument parser

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+import sys
+
+
 class LifeGame:
     def __init__(self):
-        pass
+        self.filename = None
+        self.gen_stage = None
 
     def run(self):
-        pass
+        try:
+            if len(sys.argv) == 1:
+                pass
+            elif len(sys.argv) <= 3:
+                self.filename = sys.argv[1]
+                if '.txt' not in self.filename:
+                    print("You can use only txt format file.")
+                    return
+                if len(sys.argv) == 3:
+                    try:
+                        self.gen_stage = int(sys.argv[2])
+                        return
+                    except Exception as e:
+                        raise e
+            else:
+                print("Check the program usage.")
+
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
